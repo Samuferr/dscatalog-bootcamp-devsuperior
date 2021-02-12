54eaa5f16aeed0d615b6045cc8fa662905dc271e
Samuel Ferreira <samuferr@gmail.com>
2021-02-12 12:22:56 -0300

    Add product listing with backend

---
+++ front-web/src/pages/Catalog/index.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { ProductsResponse } from '../../core/types/Product';
+import { makeRequest } from '../../core/utils/request';
+import ProductCard from './components/ProductCard';
+import './styles.scss';
+
+const Catalog = () => {
+    const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+
+    
+    useEffect(() => {
+    const params = {
+        page: 0,
+        linesPerPage: 12,
+    }
+
+    makeRequest({ url: '/products', params })
+    .then (response => setProductsResponse(response.data));
+    }, []);
+
+return (
+ <div className="catalog-container">
+        <h1 className="catalog-title">Catálogo de Produtos</h1>
+    <div className="catalog-products">
+        {productsResponse?.content.map(product =>  (
+            <Link to = {`/products/${product.id}`} key = {product.id}>
+                <ProductCard product={product} />
+            </Link>
+        ))}
+    </div>
+ </div>
+    )
+};
+
+export default Catalog;
